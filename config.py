c099473782a5d0e031aa8299436045dfe42956db
brokenstan039 <112828690+brokenstan039@users.noreply.github.com>
2022-12-13 12:34:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-BOT_ID = getenv("BOT_ID", "")#token first 10 digits
-AI_API_KEY = getenv("AI_API_KEY", "")
-AI_ID = int(getenv("AI_ID", ""))
+BOT_TOKEN = getenv("BOT_TOKEN", "5848010202:AAHBhS7qdzy52OHpDcI4z33FSrQP78fN_Mk")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://TITAN:<password>@cluster0.xud0wjz.mongodb.net/?retryWrites=true&w=majority")
+BOT_ID = getenv("BOT_ID", "5848010202")#token first 10 digits
+AI_API_KEY = getenv("AI_API_KEY", "yYN0c1iYClTzcmAl")
+AI_ID = int(getenv("AI_ID", "171135"))
